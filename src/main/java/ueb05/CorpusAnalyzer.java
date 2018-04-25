26d18a346360b8aba3dddcb18925ee88cc2296bc
ThimoBauer <37835139+ThimoBauer@users.noreply.github.com>
2018-04-25 10:29:50 +0200

    wird schon

--- src/main/java/ueb05/CorpusAnalyzer.java
+++ src/main/java/ueb05/CorpusAnalyzer.java
@@ -6,25 +6,25 @@ class CorpusAnalyzer {
 	private List<String> theses;
 
 	CorpusAnalyzer(Iterator<String> thesesIterator) {
-		while(thesesIterator.hasNext()){
+		while (thesesIterator.hasNext()) {
 			theses.add(thesesIterator.next());
 		}
 	}
 
 
 	int countTheses() {
-		return theses.size(); 	//Gibt die Anzahl der angefertigten Theses zurück
+		return theses.size();    //Gibt die Anzahl der angefertigten Theses zurück
 	}
 
 
 	int averageThesisTitleLength() {
 		int average = 0;
-		for (String s: theses)						//Gibt die durchschnittliche Länge von Titeln in Worten zurück
+		for (String s : theses)                        //Gibt die durchschnittliche Länge von Titeln in Worten zurück
 			average = average + s.split(" ").length;
 
-		return average /theses.size();
-		}
+		return average / theses.size();
 	}
+
 
 	/**
 	 * Gibt eine alphabetisch absteigend sortierte und duplikatfreie
@@ -32,21 +32,27 @@ class CorpusAnalyzer {
 	 */
 	List<String> uniqueFirstWords() {
 		List<String> list = new LinkedList<>();
-	}
+		for (String s : theses) {
+			String[] t = s.split(" ");
+			list.add(t[0]);
+			if (list.contains(t[0]))
+				list.add(t[0]);
+		}
 
-	/**
-	 * Gibt einen Iterator auf Titel zurück; dabei werden alle Woerter, welche
-	 * in `blackList` vorkommen durch Sternchen ersetzt (so viele * wie Buchstaben).
-	 */
-	Iterator<String> censoredIterator(Set<String> blackList) {
-		throw new UnsupportedOperationException();
-	}
+		/**
+		 * Gibt einen Iterator auf Titel zurück; dabei werden alle Woerter, welche
+		 * in `blackList` vorkommen durch Sternchen ersetzt (so viele * wie Buchstaben).
+		 */
+		Iterator<String> censoredIterator (Set < String > blackList) {
+			throw new UnsupportedOperationException();
+		}
 
-	/**
-	 * Gibt eine Liste von allen Titeln zurueck, wobei Woerter so ersetzt werden,
-	 * wie sie in der Map abgebildet werden.
-	 */
-	List<String> normalizedTheses(Map<String, String> replace) {
-		throw new UnsupportedOperationException();
+		/**
+		 * Gibt eine Liste von allen Titeln zurueck, wobei Woerter so ersetzt werden,
+		 * wie sie in der Map abgebildet werden.
+		 */
+		List<String> normalizedTheses (Map < String, String > replace){
+			throw new UnsupportedOperationException();
+		}
 	}
 }
